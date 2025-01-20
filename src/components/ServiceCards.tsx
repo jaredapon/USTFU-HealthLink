9a3c359718a001428d5bd378bb7c66be19727ed0
Hazel Mae Bea <117628698+HazelMaeBea@users.noreply.github.com>
2025-01-21 01:23:17 +0800

    added in images for the forms

--- src/components/ServiceCards.tsx
+++ src/components/ServiceCards.tsx
@@ -1,25 +1,33 @@
 import React from "react";
 import "./ServiceCards.css";
+import inpatientIcon from "../assets/inpatient.png";
+import admissionIcon from "../assets/admission.png";
+import outpatientIcon from "../assets/outpatient.png";
+import eyeglassesIcon from "../assets/eyeglasses.png";
 
 const ServiceCards: React.FC = () => {
-  const cards = [
-    { title: "Inpatient Application Form", icon: "📝" },
-    { title: "Admission Application Form", icon: "🏥" },
-    { title: "Outpatient Application Form", icon: "📄" },
-    { title: "Eyeglasses Reimbursement Form", icon: "👓" },
-  ];
+	const cards = [
+		{ title: "Inpatient Application Form", icon: inpatientIcon },
+		{ title: "Admission Application Form", icon: admissionIcon },
+		{ title: "Outpatient Application Form", icon: outpatientIcon },
+		{ title: "Eyeglasses Reimbursement Form", icon: eyeglassesIcon },
+	];
 
-  
-  return (
-    <div className="service-cards">
-      {cards.map((card, index) => (
-        <div className="card" key={index}>
-          <span className="card-icon">{card.icon}</span>
-          <p>{card.title}</p>
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div className="service-cards">
+			{cards.map((card, index) => (
+				<div className="card" key={index}>
+					<img
+						className="card-icon"
+						src={card.icon}
+						alt={card.title}
+					/>
+					{/* <span className="card-icon">{card.icon}</span> */}
+					<p>{card.title}</p>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default ServiceCards;
