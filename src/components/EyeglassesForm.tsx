758591c3a7474ba411f8785b99ce507b908ebfb3
frierenpon <zanderjay.deguzman.cics@ust.edu.ph>
2025-01-22 23:21:02 +0800

    form components and header component made,,

---
+++ src/components/EyeglassesForm.tsx
@@ -0,0 +1,11 @@
+import FormHeader from "./FormHeader";
+
+const EyeglassesForm = () => {
+    return (
+      <div>
+        <FormHeader formType = "Eyeglasses Reimbursement" />
+      </div>
+    );
+}
+
+export default EyeglassesForm;
--- src/components/ServiceCards.tsx
+++ src/components/ServiceCards.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import "../css/ServiceCards.css";
 import inpatientIcon from "../assets/inpatient.png";
 import admissionIcon from "../assets/admission.png";
@@ -5,23 +6,23 @@ import outpatientIcon from "../assets/outpatient.png";
 import eyeglassesIcon from "../assets/eyeglasses.png";
 
 const ServiceCards = () => {
+	const navigate = useNavigate();
 	const cards = [
-		{ title: "Inpatient Application Form", icon: inpatientIcon },
-		{ title: "Admission Application Form", icon: admissionIcon },
-		{ title: "Outpatient Application Form", icon: outpatientIcon },
-		{ title: "Eyeglasses Reimbursement Form", icon: eyeglassesIcon },
+		{ title: "Inpatient Application Form", icon: inpatientIcon, path: "/inpatient" },
+		{ title: "Admission Application Form", icon: admissionIcon, path: "/admission" },
+		{ title: "Outpatient Application Form", icon: outpatientIcon, path: "/outpatient" },
+		{ title: "Eyeglasses Reimbursement Form", icon: eyeglassesIcon, path: "/eyeglasses" },
 	];
 
 	return (
 		<div className="service-cards">
 			{cards.map((card, index) => (
-				<div className="card" key={index}>
+				<div className="card" key={index} onClick={() => navigate(card.path)}>
 					<img
 						className="card-icon"
 						src={card.icon}
 						alt={card.title}
 					/>
-					{/* <span className="card-icon">{card.icon}</span> */}
 					<p>{card.title}</p>
 				</div>
 			))}
