6f268ae6f2dd99af9610c53f415d903a57c13840
Hazel Mae Bea <117628698+HazelMaeBea@users.noreply.github.com>
2025-01-23 21:16:20 +0800

    Changed Directory and css import link

--- src/components/Form Application/FormHeader.tsx
+++ src/components/Form Application/FormHeader.tsx
@@ -1,23 +1,33 @@
-import "../css/FormHeader.css";
+import "../FormHeader.css";
 import USTFULogo from "../images/USTFULogo.png";
 import USTLogo from "../images/USTLogo.png";
 
 interface FormHeaderProps {
-    formType: string;
+	formType: string;
 }
 
 const FormHeader = ({ formType }: FormHeaderProps) => {
-    return (
-        <div className="form-header">
-            <img id="USTFU-logo" src={USTFULogo} alt="USTFU Logo" className="logo" />
-            <div className="header-text">
-                <h1>Hospitalization and Medical Benefits Fund</h1>
-                <h1>Application Form</h1>
-                <h1>{formType}</h1>
-            </div>
-            <img id="USTFU-logo" src={USTLogo} alt="UST Logo" className="logo" />
-        </div>
-    );
-}
+	return (
+		<div className="form-header">
+			<img
+				id="USTFU-logo"
+				src={USTFULogo}
+				alt="USTFU Logo"
+				className="logo"
+			/>
+			<div className="header-text">
+				<h1>Hospitalization and Medical Benefits Fund</h1>
+				<h1>Application Form</h1>
+				<h1>{formType}</h1>
+			</div>
+			<img
+				id="USTFU-logo"
+				src={USTLogo}
+				alt="UST Logo"
+				className="logo"
+			/>
+		</div>
+	);
+};
 
 export default FormHeader;
