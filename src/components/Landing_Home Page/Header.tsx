f8ea68c619ef5815e3a8e2a035378092acf4f38b
Hazel Mae Bea <117628698+HazelMaeBea@users.noreply.github.com>
2025-01-23 21:02:04 +0800

    move files to followSOC

---
+++ src/components/Landing_Home Page/Header.tsx
@@ -0,0 +1,12 @@
+import "../Header.css";
+
+const Header = () => {
+	return (
+		<div className="Header">
+			<h1>USTFU HealthLink</h1>
+			{/*placeholder only ^^ put actual logo later on  */}
+		</div>
+	);
+};
+
+export default Header;
